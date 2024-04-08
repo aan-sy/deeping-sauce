636825249d8634746d22ecbfacee0c65aa83a871
soyeon <soylee57@naver.com>
2024-04-08 21:45:07 +0900

    Header: 스크롤 시 나타내고 숨기기, 모바일에서 메뉴 토글

---
+++ src/active-menu.js
@@ -0,0 +1,53 @@
+const header = document.querySelector('.header');
+const headerHeight = header.getBoundingClientRect().height;
+const homeSection = document.querySelector('.home');
+const homeHeight = homeSection.getBoundingClientRect().height;
+const toggleBtn = document.querySelector('.toggle-menu');
+const menu = document.querySelector('header .menu');
+
+let prevY = 0;
+let currentY = 0;
+let isScrollDown = false;
+
+const checkScrollDown = () => {
+  currentY = scrollY;
+  isScrollDown = prevY < currentY ? true : false;
+  prevY = currentY;
+}
+
+const show = () => {  
+  if(isScrollDown && (currentY >= homeHeight) && !menu.classList.contains('open')) {
+    header.style.transform = `translateY(-${headerHeight}px)`
+  } else if(!isScrollDown) {
+    header.style.transform = 'translateY(0)';
+  }
+}
+
+const makeDarker = () => {
+  if(isScrollDown && (currentY >= headerHeight)) {
+    header.classList.add('dark');
+  } else if(!isScrollDown && (currentY < headerHeight)) {
+    header.classList.remove('dark');
+  }
+}
+
+document.addEventListener('scroll', () => {
+  checkScrollDown();
+  show();
+  makeDarker();
+})
+
+// 모바일에서 nav 메뉴 토글
+toggleBtn.addEventListener('click', () => {
+  menu.classList.toggle('open');
+  if(menu.classList.contains('open')) {
+    toggleBtn.innerHTML = '<i class="fa-solid fa-minus"></i>';
+  } else {
+    toggleBtn.innerHTML = '<i class="fa-solid fa-plus"></i>';
+  }
+})
+
+menu.addEventListener('click', e => {
+  menu.classList.remove('open');
+  toggleBtn.innerHTML = '<i class="fa-solid fa-plus"></i>';
+})
